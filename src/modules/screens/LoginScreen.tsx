75322bfc2a30e4a9ea7ea0b6b4e21540bc8a796e
JOANNE EMILLY SANTOS DA COSTA <124714961+Joanne-Emilly@users.noreply.github.com>
2025-01-29 09:49:01 -0300

    feat: Create login component

--- src/modules/screens/LoginScreen.tsx
+++ src/modules/screens/LoginScreen.tsx
@@ -1,9 +1,14 @@
-import { BackgroundImage } from '../styles/loginScreen.styles';
+import { BackgroundImage, ContainerLogin, LimitedContainer, LogoImage } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
   return (
     <div>
       <BackgroundImage src="./background.png" />
+      <ContainerLogin>
+        <LimitedContainer>
+          <LogoImage src="./logoImage.png" />
+        </LimitedContainer>
+      </ContainerLogin>
     </div>
   );
 };
--- src/modules/styles/loginScreen.styles.ts
+++ src/modules/styles/loginScreen.styles.ts
@@ -8,3 +8,29 @@ export const BackgroundImage = styled.img`
   height: 100vh;
   object-fit: cover;
 `;
+
+export const LogoImage = styled.img`
+  max-width: 300px;
+`;
+
+export const ContainerLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  right: 0;
+  top: 0;
+  background-color: #9ed4f1;
+  width: 100%;
+  height: 100vh;
+  max-width: 646px;
+  padding: 22px;
+`;
+
+export const LimitedContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 498px;
+`;
